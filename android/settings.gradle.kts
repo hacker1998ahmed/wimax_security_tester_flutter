a9ffb84ceb70454df0badce18d5a0600379e87ec
ahmed mostafa <gogom8870@gmail.com>
2025-06-15 14:04:35 +0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -4,31 +4,14 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-
-    // حل مشكلة Flutter SDK Path لنظام Windows
-    val flutterSdkPath: String = System.getenv("FLUTTER_SDK") ?: run {
-        val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        properties.getProperty("flutter.sdk")?.replace("\\", "/")
-            ?: throw GradleException("flutter.sdk not set in local.properties")
-    }
-
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-}
-
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.1.0" apply false  // محدث لتتوافق مع Gradle 8.1
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false  // محدث لتتوافق مع Kotlin 1.9
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
 
-rootProject.name = "Your_Project_Name"  // استبدل باسم مشروعك
-include(":app")
+include ':app'
